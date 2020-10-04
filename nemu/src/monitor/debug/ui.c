d197d088f5cdf665a757b7c2cc279784d0d0fcaa
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 18:57:40 -0700

    >  compile NEMU 3019244244 zhengmh Linux ubuntu 5.3.0-51-generic #44~18.04.2-Ubuntu SMP Thu Apr 23 14:27:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:57:40 up 1 day, 23 min,  3 users,  load average: 0.90, 0.85, 0.70 18122d6ad42c0e36629f94a9f481e1adcce31a4

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -64,7 +64,7 @@ static int cmd_info(char *args){
 	int i;
         if(strcmp(arg,"r")==0){
 		for(i=R_EAX; i<=R_EDI;i++){
-        	    printf("%s,%x",regsl[i], reg_l(i));
+        	    printf("%s,0x%x\n", regsl[i], reg_l(i));
 		}
 		printf("eip,%x",cpu.eip);
 	}
